93439d76993c0bfdc40093d324e920e3df496af8
mayaprado <32017216+mayaprado@users.noreply.github.com>
2018-04-12 11:55:10 -0400

    connecting to database and retrieving information about a film by id

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const sqlite = require('sqlite'),
+      sqlite3 = require('sqlite3'),
       Sequelize = require('sequelize'),
       request = require('request'),
       express = require('express'),
       app = express();
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+
+let db = new sqlite3.Database('./db/database.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to SQlite database.');
+});
 
 // START SERVER
 Promise.resolve()
@@ -15,8 +23,20 @@ Promise.resolve()
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
-function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+function getFilmRecommendations(req, res, next) {
+  const id = req.params.id;
+  let sql = `SELECT Title title
+            FROM films
+            WHERE films.id = ${id}
+            ORDER BY Title`;
+
+  db.each(sql, (err, row) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`${row.title}`, id);
+  console.log('id is', id);
+  });  
 }
 
 module.exports = app;
